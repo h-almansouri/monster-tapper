1fc7571a9737b50cc94295f39cc5f6186fb5a687
Jordan Yee <jordanyee799@gmail.com>
2021-11-19 10:49:15 -0800

    improved player animation

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,36 @@
+import styled, { keyframes } from "styled-components";
+
+const attack = keyframes`
+    0% {
+        transform: translate(0px, 0px)
+    }
+    25% {
+        transform: translate(-100px, -50px)
+    }
+    50% {
+        transform: translate(-200px, 0px)
+    }
+    75% {
+        transform: translate(-100px, -50px)
+    }
+    100% {
+        transform: translate(0px, 0px);
+    }
+
+`
+
 function Player({playerData, isAttacking, updatedInfo}) {
-    let playerStyle = isAttacking ? {
-        transform: 'translate(-200px, 0px)'
-    } : null;
+    // let playerStyle = isAttacking ? {
+    //     transform: 'translate(-200px, 0px)'
+    // } : null;
+    
     return (
-        <div className='player' style={playerStyle}>
+        isAttacking ?
+        <PlayerCont className='player'>
+            <img className={`weapon ${updatedInfo.weaponName}`} src={updatedInfo.weapon} />
+            <img className='hero' src={playerData.image}/>
+        </PlayerCont> :
+        <div className='player'>
             <img className={`weapon ${updatedInfo.weaponName}`} src={updatedInfo.weapon} />
             <img className='hero' src={playerData.image}/>
         </div>
@@ -11,3 +38,9 @@ function Player({playerData, isAttacking, updatedInfo}) {
 }
 
 export default Player
+
+const PlayerCont = styled.div`
+    animation-name: ${attack};
+    animation-duration: 0.2s;
+    animation-iteration-count: 1;
+`
